631601688033f73317cfeacd4f695968b76c6536
Yhair <bryamyhairperezdiaz@gmail.com>
2018-08-02 07:33:13 -0500

    winnerLength function, get truffle accounts

--- contracts/quiniela.sol
+++ contracts/quiniela.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.24;
 
-
+/** @title quiniela. */
 contract quiniela{
     
     // SRUCTS.
 
-    struct user{
-        bool used;
+    struct user{ // Struct of the player.
+        bool used; // If this addres is used.
         uint place1; // Id of team 1.
         uint place2; // Id of team 2.
         uint place3; // Id of team 3.
@@ -19,140 +19,140 @@ contract quiniela{
     // TYPES.
 
     address [] public winners; // Array of winners.
-    bool public toVote;
-    uint public votes;
-    
+    bool public toVote; 
+    uint public votes; // Count of number of votes.
     address [] public players; // Array of participants.
     mapping (uint => string) public teams; // Id of each team.
-    address public constant admin= 0x17d98914dBf1035d808ba2BCa60652F8822b42D4; //Admin of the contract.
+    address public constant admin = 0x17d98914dBf1035d808ba2BCa60652F8822b42D4; //Admin of the contract. accounts[9] of truffle.
     mapping (address => user) public users;
-    uint public constant cost= .2 ether;
+    uint public constant cost = 0.2 ether; // Cost tu enter in the football pool.
 
-    // MODIFIERS
+    // MODIFIERS.
 
     // Check if the sender is the admin of the contract.
     modifier onlyAdmin() {
-        require(msg.sender == admin);
+        require( msg.sender == admin );
         _;
     }
+
+    // CONSTRUCTOR.
     
     /**
     * @dev This function is executed at initialization and sets the team of the football pool.
     */
     constructor() public {
-        teams[1]= "Germany";
-        teams[2]= "Argentina";
-        teams[3]= "Australia";
-        teams[4]= "Belgium";
-        teams[5]= "Brazil";
-        teams[6]= "Colombia";
-        teams[7]= "Costa Rica";
-        teams[8]= "Croatia";
-        teams[9]= "Denmark";
-        teams[10]= "Egypt";
-        teams[11]= "England";
-        teams[12]= "France";
-        teams[13]= "Iceland";
-        teams[14]= "Ir Iran";
-        teams[15]= "Japan";
-        teams[16]= "Korea Republic";
-        teams[17]= "Mexico";
-        teams[18]= "Morocco";
-        teams[19]= "Nigeria";
-        teams[20]= "Panama";
-        teams[21]= "Peru";
-        teams[22]= "Poland";
-        teams[23]= "Portugal";
-        teams[24]= "Russia";
-        teams[25]= "Saudi Arabia";
-        teams[26]= "Senegal";
-        teams[27]= "Serbia";
-        teams[28]= "Spain";
-        teams[29]= "Sweden";
-        teams[30]= "Switzerland";
-        teams[31]= "Tunisia";
-        teams[32]= "Uruguay";
+        teams[1] = "Germany";
+        teams[2] = "Argentina";
+        teams[3] = "Australia";
+        teams[4] = "Belgium";
+        teams[5] = "Brazil";
+        teams[6] = "Colombia";
+        teams[7] = "Costa Rica";
+        teams[8] = "Croatia";
+        teams[9] = "Denmark";
+        teams[10] = "Egypt";
+        teams[11] = "England";
+        teams[12] = "France";
+        teams[13] = "Iceland";
+        teams[14] = "Ir Iran";
+        teams[15] = "Japan";
+        teams[16] = "Korea Republic";
+        teams[17] = "Mexico";
+        teams[18] = "Morocco";
+        teams[19] = "Nigeria";
+        teams[20] = "Panama";
+        teams[21] = "Peru";
+        teams[22] = "Poland";
+        teams[23] = "Portugal";
+        teams[24] = "Russia";
+        teams[25] =  "Saudi Arabia";
+        teams[26] = "Senegal";
+        teams[27] = "Serbia";
+        teams[28] = "Spain";
+        teams[29] = "Sweden";
+        teams[30] = "Switzerland";
+        teams[31] = "Tunisia";
+        teams[32] = "Uruguay";
     }
 
     // PUBLIC METHODSS
 
     /**
     * @dev Function...
-    * @param _place1 -
-    * @param _place1 -
-    * @param _place1 -
-    * @param _name - 
-    * @param _password -
+    * @param _place1 - Uint - To set the place 1.
+    * @param _place1 - Uint - To set the place 2.
+    * @param _place1 - Uint - To set the place 3.
+    * @param _name - String - To set the name.
+    * @param _password - String - Password of the football pool.
     */
-    function addUser(uint _place1, uint _place2, uint _place3, string _name, string _password)public  payable {
-        require(test(_password)); // #crypto_Polla2018
-        require(!users[msg.sender].used);
-        users[msg.sender].place1=_place1;
-        users[msg.sender].place2=_place2;
-        users[msg.sender].place3=_place3;
-        
-        users[msg.sender].name=_name;
-        users[msg.sender].date=now;
-        users[msg.sender].used=true;
-        players.push(msg.sender);
+    function addUser ( uint _place1, uint _place2, uint _place3, string _name, string _password ) public  payable {
+        require( isPassword( _password ) ); // #crypto_Polla2018
+        require( !users[msg.sender].used );
+        require( cost <= msg.value );
+        users[msg.sender].place1 = _place1; // Set _place1.
+        users[msg.sender].place2 = _place2; // Set _place2.
+        users[msg.sender].place3 = _place3; // Set _place3.
+        users[msg.sender].name = _name; // Set name.
+        users[msg.sender].date = now; // Set timestamp.
+        users[msg.sender].used = true; 
+        players.push(msg.sender); // Push to the array.
     }
     
     /**
-    * @dev Function...
+    * @dev Function to set the winners to the winners array.
     */
-    function setWinners(address [] _winners) public onlyAdmin(){
-       winners= _winners;
-       toVote= true;
+    function setWinners ( address [] _winners ) public onlyAdmin() {
+       winners = _winners;
+       toVote = true;
     }
     
     /**
-    * @dev Function..
+    * @dev Function to vote. Pay the money if more than 50% of the particiants are agree.
     */
-    function vote(bool _vote) public returns (bool) {
-        require(toVote);
-        require(users[msg.sender].voted == false);
-        users[msg.sender].voted=true;
-        
-        if(_vote){
-            votes+=1;
-            if(votes > (playersLength()/2)){
-                payMoney();
+    function vote ( bool _vote ) public returns ( bool ) {
+        require( toVote ); // The participant can vote.
+        require( users[msg.sender].voted == false ); // The participant vote.
+        users[msg.sender].voted = true;
+        if( _vote ) {
+            votes += 1; // Increment in 1 the votes.
+            if( votes > ( playersLength() / 2 ) ) { //
+                payMoney(); // Function.
             }
         }
         return true;
     }
     
     /**
-    * @dev Function...
+    * @dev Function to pay the money to each winner.
     */
-    function payMoney() private {
-        uint toPay= address(this).balance/(winners.length);
-        
-        for(uint x = 0; x < winners.length - 1 ; x++ ){
-            winners[x].transfer(toPay);
+    function payMoney () private {
+        uint toPay= address( this ).balance / winnersLength(); // Ether assigned for each winner.
+
+        for( uint x = 0; x < winnersLength() - 1 ; x++ ) {
+            winners[x].transfer( toPay ); // Transfer ether to the winner.
         }
-        winners[(winners.length)-1].transfer(address(this).balance);
+        winners[ winnersLength() - 1 ].transfer( address( this ).balance ); // Transfer the rest of the ether.
     }
     
     /**
-    * @dev Function...
+    * @dev Function to get players array length.
     */
-    function playersLength() public constant returns(uint){
+    function playersLength () public constant returns ( uint ) {
         return players.length;
-    }
-    
-    /**
-    * @dev Function to recover the funds on the contract.
-    */
-    function kill() public onlyAdmin() {
-        selfdestruct(admin);
     }
 
     /**
-    * @dev Function to get the winners array.
+    * @dev Function to get winners array length.
     */
-    function getWinners () public constant returns( address []) {
-        return winners;
+    function winnersLength () public constant returns ( uint ) {
+        return winners.length;
+    }
+    
+    /**
+    * @dev Function to recover the funds of the contract.
+    */
+    function kill () public onlyAdmin() {
+        selfdestruct( admin );
     }
 
     // PRIVATE METHODS
@@ -161,9 +161,8 @@ contract quiniela{
     * @dev Function to compare the password.
     * @return Bool - Valid password.
     */
-    function test(string _cad) private pure returns(bool){
-        return keccak256(_cad) == 0x2a56a8951ea80f8d88e626b0e9632a3c2fe7c1f6f450a8838e03734380d1c049;
+    function isPassword( string _cad ) private pure returns ( bool ) {
+        return keccak256( abi.encodePacked( _cad ) ) == 0x2a56a8951ea80f8d88e626b0e9632a3c2fe7c1f6f450a8838e03734380d1c049;
     }
-    
     
 }
--- test/quinela.js
+++ test/quinela.js
@@ -5,78 +5,69 @@ contract('Quinela', function(accounts) {
   it("Creating the instance ( test )", () => {
 		return Quinela.deployed()
     	.then( instance => {
-        quinelaInstance = instance;
+        quinelaInstance = instance; // Instance of the contract.
     	})
   });
   it("addUser test function", () => {
     // Function send.
-    var _name = "nameTest";
-    var _password = "#crypto_Polla2018";
-    return quinelaInstance.addUser( 1, 2, 3, _name, _password, { from: '0xb794faC539480856cF99D216bA96181A83717670', value: web3._extend.utils.toWei( '0.2', "ether") } )
+    var _name = "nameTest"; // Name for test.
+    var _password = "#crypto_Polla2018"; // Password of the footbal pool.
+    return quinelaInstance.addUser( 1, 2, 3, _name, _password, { from: accounts[0], value: web3._extend.utils.toWei( '0.2', "ether") } )
       .then( () => {
-        return quinelaInstance.addUser( 4, 5, 6, _name, _password, { from: '0xA1b56Df2FAF9F18047311d456c7abC6Daba1e94E', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 4, 5, 6, _name, _password, { from: accounts[1], value: web3._extend.utils.toWei( '0.2', "ether") } );
       })
       .then( () => {
-        return quinelaInstance.addUser( 7, 8, 9, _name, _password, { from: '0x44DADDaeacF5a31CFa3653013E1E96A8fb8b46fe', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 7, 8, 9, _name, _password, { from: accounts[2], value: web3._extend.utils.toWei( '0.2', "ether") } );
       })
       .then( () => {
-        return quinelaInstance.addUser( 10, 11, 12, _name, _password, { from: '0x75BC560F4fa4633a3fd177E51922a974f1aCc383', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 10, 11, 12, _name, _password, { from: accounts[3], value: web3._extend.utils.toWei( '0.2', "ether") } );
       })
       .then( () => {
-        return quinelaInstance.addUser( 13, 14, 15, _name, _password, { from: '0x4F15CCF21c0ADc944aAf99A01220763baBdDBB41', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 13, 14, 15, _name, _password, { from: accounts[4], value: web3._extend.utils.toWei( '0.2', "ether") } );
       })
       .then( () => {
-        return quinelaInstance.addUser( 16, 17, 18, _name, _password, { from: '0x8588B40EF767a597913207DF57b4A3B227E197A3', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 16, 17, 18, _name, _password, { from: accounts[5], value: web3._extend.utils.toWei( '0.2', "ether") } );
       })
       .then( () => {
-        return quinelaInstance.addUser( 19, 20, 21, _name, _password, { from: '0x5c96953A1D457203b60F7906CaF3f519FEE6920f', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 19, 20, 21, _name, _password, { from: accounts[6], value: web3._extend.utils.toWei( '0.2', "ether") } );
       })
       .then( () => {
-        return quinelaInstance.addUser( 22, 23, 24, _name, _password, { from: '0x230ecf62B8Ba553E922CEe21aBa1A8cC02302519', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 22, 23, 24, _name, _password, { from: accounts[7], value: web3._extend.utils.toWei( '0.2', "ether") } );
       })
       .then( () => {
-        return quinelaInstance.addUser( 25, 26, 27, _name, _password, { from: '0xAF9E1d3F3588d36301354544C908F6B69Cd4B132', value: web3._extend.utils.toWei( '0.2', "ether") } );
+        return quinelaInstance.addUser( 25, 26, 27, _name, _password, { from: accounts[8], value: web3._extend.utils.toWei( '0.2', "ether") } );
       });
 
   });
   
   it( "setWinners test function", () => {
-    var _winners = [ '0xb794faC539480856cF99D216bA96181A83717670', "0xA1b56Df2FAF9F18047311d456c7abC6Daba1e94E" ];
-    return quinelaInstance.setWinners( _winners, { from: '0x17d98914dBf1035d808ba2BCa60652F8822b42D4' } );
+    var _winners = [ accounts[0] ]; // Address array of the winners
+    return quinelaInstance.setWinners( _winners, { from: accounts[9] } );
   });
   
-  it( "vote test function ",() => {
+  it( "vote test function ",() => { // 
     var _vote = true;
-    return quinelaInstance.vote( _vote, { from: '0xb794faC539480856cF99D216bA96181A83717670' } )
+    return quinelaInstance.vote( _vote, { from: accounts[0] } )
       .then( () => {
-        return quinelaInstance.vote( _vote, { from: '0xA1b56Df2FAF9F18047311d456c7abC6Daba1e94E' } );
+        return quinelaInstance.vote( _vote, { from: accounts[1] } );
       })
       .then( () => {
-        return quinelaInstance.vote( _vote, { from: '0x44DADDaeacF5a31CFa3653013E1E96A8fb8b46fe' } );
+        return quinelaInstance.vote( _vote, { from: accounts[2] } );
       })
       .then( () => {
-        return quinelaInstance.vote( _vote, { from: '0x75BC560F4fa4633a3fd177E51922a974f1aCc383' } );
+        return quinelaInstance.vote( _vote, { from: accounts[3] } );
       })
       .then( () => {
-        return quinelaInstance.vote( _vote, { from: '0x4F15CCF21c0ADc944aAf99A01220763baBdDBB41' } );
+        return quinelaInstance.vote( _vote, { from: accounts[4] } );
       });
   });
   
   it( "playersLength test function" , () => {
     return quinelaInstance.playersLength.call()
     .then( length => {
-    	console.log( length.toNumber() );
+    	console.log( "Players: " + length.toNumber() ); // Numbers of players.
     	//assert.equal( length.toNumber(), expected, '');
     });
   });
-
-  //Get Array of winners.
-  it("getWinners test", () => {
-    return quinelaInstance.getWinners.call()
-    .then( address => {
-      console.log( address );
-    });
-  });
-  
 
 });
